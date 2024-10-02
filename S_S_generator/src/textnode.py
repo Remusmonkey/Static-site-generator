5b1488ebd606ba2b6c44cfb54b84f3a873d991fa
Remusmonkey <127868049+Remusmonkey@users.noreply.github.com>
2024-10-02 00:08:41 -0700

    Added the text types to textnode.py They were present in the solution from Boot.dev However they were not mentioned in the instructions in the lesson These were added: text_type_text = "text" text_type_bold = "bold" text_type_italic = "italic" text_type_code = "code" text_type_link = "link" text_type_image = "image"

--- S_S_generator/src/textnode.py
+++ S_S_generator/src/textnode.py
@@ -1,3 +1,11 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, node_text, text_type, url=None):
         self.text = node_text
