3641e7fe1c19bb5b161a4de6f2c7347d7482d8fb
Remusmonkey <127868049+Remusmonkey@users.noreply.github.com>
2024-10-01 23:58:50 -0700

    Added main.py, main.sh and textnode.py ran

---
+++ S_S_generator/src/main.py
@@ -0,0 +1,11 @@
+#print("Hello World")
+
+from textnode import TextNode
+
+def main():
+    test=TextNode("This is a test/text node", "bold", "www.sweetcodeman.com")
+    print(repr(test))
+
+
+
+main()
---
+++ S_S_generator/src/textnode.py
@@ -0,0 +1,11 @@
+class TextNode:
+    def __init__(self, node_text, text_type, url=None):
+        self.text = node_text
+        self.text_type = text_type
+        self.url = url
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
