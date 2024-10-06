19483a363b79d014f15284dcc838e16125491f51
Remusmonkey <127868049+Remusmonkey@users.noreply.github.com>
2024-10-06 15:06:08 -0700

    Initial creation of Ch. 2 lesson 3

---
+++ S_S_generator/src/test_htmlnode.py
@@ -0,0 +1,7 @@
+import unittest
+
+from htmlnode import *
+
+testnode1 = HTMLNode("a", )
+
+class TestHTMLNode(unittest.TestCase):
--- S_S_generator/src/test_textnode.py
+++ S_S_generator/src/test_textnode.py
@@ -1,6 +1,13 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (TextNode, 
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
 
 
 class TestTextNode(unittest.TestCase):
@@ -8,6 +15,24 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+    def test_eq_two(self):
+        node = TextNode("This should be text", "italic","www.Bootsrules.com")
+        node2 = TextNode("This should be text", "italic","www.Bootsrules.com") 
+        self.assertEqual(node, node2)
+    def test_noteq_(self):
+        node = TextNode("This is different", "code", "www.cars.com")
+        node2 = TextNode("This is also different", "image", "www.ucla.com")
+        self.assertNotEqual(node, node2)
+    def test_eq_3(self):
+        node = TextNode("This should be text", "italic")
+        node2 = TextNode("This should be text", "italic", None) 
+        self.assertEqual(node, node2)
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+        print("TEST", repr(node))
 
 
 if __name__ == "__main__":
